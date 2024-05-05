3fd09ef384f76c6639c2444f25a9061bf918cf24
patalee narasinghe <patalee2001@gmail.com>
2024-05-05 12:05:06 +0530

    init

---
+++ src/Exception/CustomException.java
@@ -0,0 +1,8 @@
+package Exception;
+@SuppressWarnings("Serial")
+
+public class CustomException extends Exception{
+    public CustomException(String message){
+        super(message);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import Engine.Evaluvator;
+
+public class Main {
+    public static void main(String[] args) {
+    
+        String fn;
+        boolean isPrintAST=false,isPrintST=false;
+        if(args.length==0){
+            fn = "t1.txt";
+            isPrintAST = true;
+            isPrintST = true;
+        }
+        else if(args.length==3 && (
+                (args[0].equalsIgnoreCase("-ast") && args[1].equalsIgnoreCase("-st")) ||
+                        (args[0].equalsIgnoreCase("-st") && args[1].equalsIgnoreCase("-ast"))
+        )
+        )
+        {
+            fn = args[2];
+            isPrintAST=true;
+            isPrintST=true;
+        }
+        else if(args.length==2){
+            fn=args[1];
+            if(args[0].equalsIgnoreCase("-ast")){
+                isPrintAST=true;
+            }
+            else if(args[0].equalsIgnoreCase("-st")){
+                isPrintST=true;
+            }
+            else{
+                System.out.println("Invalid Arguments Passing!");
+                return;
+            }
+        }
+        else if(args.length==1){
+            fn = args[0];
+        }
+        else{
+            System.out.println("Invalid Arguments Passing!");
+            return;
+        }
+        System.out.println(Evaluvator.evaluvate(fn,isPrintAST,isPrintST));                                   // get and print the answer
+
+
+
+         }
+    }
